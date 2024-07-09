707c0fe79df386c4298968ee1aaf3b53f02b16a6
farzadnorouzi <farzadnorouzi20201399@gmail.com>
2024-07-09 12:14:13 -0700

    new code

--- src/Components/Inputs/Inputs.jsx
+++ src/Components/Inputs/Inputs.jsx
@@ -2,8 +2,12 @@
 
 function Inputs() {
     return(
-        <div>
-            <input type="text" />
+        <div class="bg-green-400 w-3/5 h-96 text-center">
+            <div> <label>invalid:<input class="mb-16 mt-12 rounded text-center" type="text" placeholder="please enter..."/></label></div>
+            <div><label>long:<input class="bg-color-500 rounded text-center" type="number" placeholder="pelease enter..." /></label></div>
+            <div><label>lat:<input class ="bg-white-500 rounded mt-2 text-center" type="number" placeholder="please enter..."/></label></div>
+            <h1 class="bg-blue-400">hello</h1>
+            <p class="bg-red-300">your weather</p>
 
         </div>
     )
--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
     return(
 
     
-      <div class="bg-red-500 text-4xl w-2/5 h-96">
+      <div class="bg-red-500 text-4xl w-2/5 h-96 text-center">
         <h1 class ="pt-48">word weather</h1>
       </div>
     )
