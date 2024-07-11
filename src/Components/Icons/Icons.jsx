9fc19c5cf4f2fb48b2c328c638502d83a77f2d57
farzadnorouzi <farzadnorouzi20201399@gmail.com>
2024-07-11 16:08:34 -0700

    create icons and inputs

---
+++ src/Components/Icons/Icons.jsx
@@ -0,0 +1,9 @@
+
+function Icons() {
+    return(
+        <h1>hello word</h1>
+    )
+    
+}
+
+export default Icons
--- src/Components/Inputs/Inputs.jsx
+++ src/Components/Inputs/Inputs.jsx
@@ -1,11 +1,16 @@
 import{ useEffect, useState } from "react";
 import axios from "axios";
-import DatePicker from "react-datepicker";
+import { format } from 'date-fns';
+import { FaCloudMoonRain } from "react-icons/fa6";
+import { IoSunny } from "react-icons/io5";
+
 
 
 function Inputs() {
 
-    const [startDate, setStartDate] = useState(new Date());
+   
+        const currentDate = new Date();
+        const formattedDate = format(currentDate, 'MMMM do, yyyy');
        
         const [Weather, setWeather] = useState(null);
         const [Input, setInput] = useState(null);
@@ -39,25 +44,39 @@ function Inputs() {
             <>
 
            
-                <div class="w-full h-96 text-center pt-20 bg-[url('https://static.vecteezy.com/system/resources/previews/006/524/224/original/weather-app-ui-ux-kit-elements-website-landing-page-vector.jpg')]">
-                    <input type="text" className="bg-withe-400 text-center rounded" onChange={handellInput} required placeholder="serch your town..." />
+                <div class="w-full h-3/4 text-center pt-20 bg-[url('https://png.pngtree.com/thumb_back/fh260/background/20201012/pngtree-white-cloud-on-blue-sky-weather-background-image_410050.jpg')]">
+                
+                   <h1 class="mb-4">welcome to my app</h1>
+
+                    <div class="w-1/2 shadow-2xl shadow-black ml-56 pt-10 rounded">
+                    <input type="text" class="bg-withe-400 text-center rounded" onChange={handellInput} required placeholder="serch your town..." />
                     <button class ="bg-yellow-200 text-center  rounded " onClick={handellClick}>submit</button>
-                <div>
-                    <p class="pt-6">{Input}</p>
-                    <p class="pt-1">{Api}=C°</p>
-                    <h1>today</h1>
-                </div>
+               
+                    <div class="text-center pt-5 rounded">
+                    <p class="pt-2">{Input}</p>
+                    <p class="pt-1">{(Api - 273.15).toFixed(2)}°C</p>
+               
+                <div class="pt-2">
+
+                   <p>{formattedDate}</p>
+
+                  </div>
+                
+            </div>
+
+            </div>
 
 
+        <div>
+            
+        {Api && (Api - 273.15) < 20 && (Api - 273.15) >= 0 && <div class="w-1/3 ml-72 mt-5 rounded"><FaCloudMoonRain class="w-96 size-48 text-center"/></div>}
+        {Api && (Api - 273.15) >= 20 && (Api - 273.15) < 50 && <div class="w-1/3 ml-72 mt-5 rounded"><IoSunny class="w-96 size-48 text-center"/></div>}
+              
+              
+              </div>
 
 
-               <div class="pt-10">
-                <DatePicker  selected={startDate} onChange={(date) => 
-                        setStartDate(date)} /> 
-                        </div>
-
-                </div>
-
+              </div>
             </>
         );
     }
